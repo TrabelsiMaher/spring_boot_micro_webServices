534be1ce3d00a426bcf8063a03e937f43ef531b8
maher <mahtrabelsi@gmail.com>
2023-11-14 22:14:22 +0100

    création du projet !!!

---
+++ src/main/java/org/microcommerce/ws/MicrocommerceWsApplication.java
@@ -0,0 +1,13 @@
+package org.microcommerce.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocommerceWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrocommerceWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/microcommerce/ws/MicrocommerceWsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.microcommerce.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrocommerceWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
